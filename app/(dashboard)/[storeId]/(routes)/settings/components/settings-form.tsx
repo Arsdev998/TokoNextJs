1f4ef78b1605a887ab9d776bf14d741537994130
Arsdev998 <arsdev998@gmail.com>
2024-07-31 15:14:26 +0700

    settings feat update and delete

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-aler";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -7,15 +9,19 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 interface SettingsFormProps {
@@ -24,12 +30,14 @@ interface SettingsFormProps {
 
 const formSchema = z.object({
   name: z.string().min(1, {
-    message: "Name is required",
+    message: "Nama Store tidak boleh kosong",
   }),
 });
 
 type SettingsFormValues = z.infer<typeof formSchema>;
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initinalData }) => {
+  const params = useParams();
+  const router = useRouter();
   const [open, setOpen] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const form = useForm<SettingsFormValues>({
@@ -38,13 +46,50 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initinalData }) => {
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success("Store Berhasil diupdate");
+    } catch (error) {
+      toast.error("cek kembali data yang diInput");
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/')
+      toast.success("Store Berhasil dihapus");
+    } catch (error) {
+      console.log(error);
+      toast.error("Gagal menghapus store");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Management Store " />
-        <Button variant={"destructive"} size={"sm"} onClick={() => {}}>
+        <Button
+          disabled={loading}
+          variant={"destructive"}
+          size={"sm"}
+          onClick={() => setOpen(true)}
+        >
           <Trash className="w-4 h-4" />
         </Button>
       </div>
@@ -60,16 +105,27 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initinalData }) => {
               name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel>Nama Store</FormLabel>
                   <FormControl>
-                    <Input placeholder="Nama Toko" disabled={loading} {...field}/>
+                    <Input
+                      placeholder="Nama Store"
+                      disabled={loading}
+                      {...field}
+                      className="w-[250px]"
+                    />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
           </div>
+          <Button disabled={loading} type="submit">
+            Save
+          </Button>
         </form>
       </Form>
+      <Separator/>
+      <ApiAlert title="PUBLIC API/URL" description="http:link-name-website-toko" variant="public"/>
     </>
   );
 };
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { Home, HomeIcon, Settings } from "lucide-react";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 
@@ -12,8 +13,15 @@ export function MainNav({
   const params = useParams();
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Dashboard",
+      icon: <HomeIcon className="h-4 w-4"/>,
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
+      icon: <Settings className="w-4 h-4" />,
       active: pathname === `/${params.storeId}/settings`,
     },
   ];
@@ -27,10 +35,13 @@ export function MainNav({
           href={route.href}
           key={route.href}
           className={cn(
-            "text-sm font-medium transition-colors hover:text-primary dark:hover:text-white",
-            route.active ? "text-black dark:text-white" : "text-muted-foreground"
+            "text-sm font-medium transition-colors hover:text-primary dark:hover:text-white flex items-center gap-x-[1px]",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
           )}
         >
+          {route.icon}
           {route.label}
         </Link>
       ))}
