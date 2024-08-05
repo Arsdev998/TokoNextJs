c6ad3a45e9a75652f0a3f2fd473407374570b320
Arsdev998 <arsdev998@gmail.com>
2024-08-05 09:32:53 +0700

    upi fix

--- app/(dashboard)/[storeId]/(routes)/banner/[bannerId]/components/banner-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/banner/[bannerId]/components/banner-form.tsx
@@ -12,7 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
-import { ImageUpload } from "@/components/ui/image-upload";
+import ImageUpload from "@/components/ui/image-upload";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useOrigin } from "@/hooks/useOrigin";
